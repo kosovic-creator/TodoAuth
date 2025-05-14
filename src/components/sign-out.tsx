8e94dd9574587b326725f28963ced2e61ad60d41
kosovic-creator <drasko.kosovic@icloud.com>
2025-05-14 06:53:19 +0200

    sign-out

--- src/components/sign-out.tsx
+++ src/components/sign-out.tsx
@@ -1,19 +1,30 @@
 "use client";
 
 import { signOut } from "next-auth/react";
-import Link from "next/link"; // Import Link from next/link
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+
 
 const SignOut = () => {
+
+
   const handleSignOut = async () => {
-    await signOut();
-    router.push('/sign-in'); // Redirect to the home page after signing out
-    router.refresh(); // Refresh the page to update the UI
-  };
-const router = useRouter();
+  try {
+    await signOut({ callbackUrl: "/sign-in" }); // NextAuth će te automatski preusmjeriti
+    // Nema potrebe za router.push
+  } catch (error) {
+    console.error("Error during sign-out:", error);
+  }
+};
+
   return (
-    <div >
-      <Link href="" onClick={(e) => { e.preventDefault(); handleSignOut(); }}>
+    <div>
+      <Link
+        href="/sign-in"
+        onClick={(e) => {
+          e.preventDefault();
+          handleSignOut();
+        }}
+      >
         Odjavi se
       </Link>
     </div>
